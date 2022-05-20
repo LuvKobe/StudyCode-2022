81dd5d2b4e20b341183693d46c602ec61caa7946
Edison <3078601436@qq.com>
2022-05-20 16:50:59 +0800

    Code demo

---
+++ test_05_20/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+int main()
+{
+	long id = 0;
+	float c = 0.0;
+	float math = 0.0;
+	float english = 0.0;
+	scanf("%ld;%f,%f,%f",&id, &c, &math, &english);
+	printf("The each subject score of No. %ld is %.2f, %.2f, %.2f.\n",  id, c, math,english);
+	return 0;
+}*/
+
+
