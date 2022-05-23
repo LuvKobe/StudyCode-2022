0ebb7fc6b99c14c08333f281b044239dcbb413ee
Edison <3078601436@qq.com>
2022-05-23 11:50:44 +0800

    Code demo

---
+++ test_05_23/test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+int main()
+{
+	char ch = 0;
+	scanf("%c", &ch);
+	int i = 0;
+	for (i = 0; i < 5; i++) {
+		//»»ÐÐ
+		int k = 0;
+		for (k = 1; k < (5 - i); k++) {
+			printf(" ");
+		}
+
+		int j = 0;
+		for (j = 0; j <= i; j++) {
+			printf("%c ", ch);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+*/
