95f8b6b0f0c9768976d9942b4dd85323876eb078
Edison <3078601436@qq.com>
2022-06-14 22:47:52 +0800

    Code Demo

--- test_06_14/test.c
+++ test_06_14/test.c
@@ -3,6 +3,7 @@
 
 
 //喝汽水问题
+/*
 int main()
 {
 	int money; //钱
@@ -22,5 +23,68 @@ int main()
 	printf("您可以喝 %d 瓶汽水\n", total);
 	return 0;
 }
+*/
 
 
+//判断大小端
+//方法一
+/*
+int main()
+{
+	int a = 1;
+	char* p = (char*)&a;
+	if (1 == *p) {
+		printf("小端\n");
+	}
+	else {
+		printf("大端\n");
+	}
+	return 0;
+}*/
+
+
+//方法二
+/*
+int check_sys() {
+	int a = 1;
+	char* p = (char*)&a;
+	if (1 == *p) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int main()
+{
+	// 返回 1 是小端
+	// 返回 0 是大端
+	int ret = check_sys();
+	if (1 == ret) {
+		printf("小端\n");
+	}
+	else {
+		printf("大端\n");
+	}
+	return 0;
+}
+*/
+
+//方法三
+/*
+int check_sys() {
+	int a = 1;
+	char* p = (char*)&a;
+	return *p;
+}
+*/
+
+
+//方法四
+/*
+int check_sys() {
+	int a = 1;
+	return *(char*)&a;
+}
+*/
