7b543cd3105acc26a198447844ec38bd26cf07c6
Edison <3078601436@qq.com>
2022-07-03 14:54:17 +0800

    Code Demo

---
+++ test_07_04/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//struct Stu
+//{
+//	char name[20]; //姓名
+//	char sex[5]; //性别
+//	int age; //年龄
+//	int hight; //身高
+//}s2, s3, s4; //他们是全局变量
+//
+//int main()
+//{
+//	struct Stu s1; //s1 是结构体变量
+//	return 0;
+//}
+
+//调试：Ctrl+F7
+//struct Node
+//{
+//	int data;
+//	struct Node* next;
+//};
+//
+//int main()
+//{
+//
+//	return 0;
+//}
+
+
+//通讯录
+//1. 可以存放 1000 个用户的信息
+//2. 用户的信息包含：姓名、年龄、电话、住址、性别
+//3. 增加联系人
+//4. 删除联系人
+//5. 查找联系人
+//6. 修改联系人
+//7. 排序
+
