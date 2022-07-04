169e770bba5cd4551de9ed2c4741a5054b955372
Edison <3078601436@qq.com>
2022-07-04 18:30:50 +0800

    Code Demo

--- test_07_04/test.c
+++ test_07_04/test.c
@@ -1,41 +1,103 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+
+
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
-//struct Stu
-//{
-//	char name[20]; //姓名
-//	char sex[5]; //性别
-//	int age; //年龄
-//	int hight; //身高
-//}s2, s3, s4; //他们是全局变量
+//int my_strlen(const char* str) {
+//	assert(str);
+//	int cnt = 0;
+//	while (*str != '\0') {
+//		cnt++;
+//		str++;
+//	}
+//	return cnt;
+//}
+
+//
+//int my_strlen(const char* str) {
+//	assert(str);
+//	if (*str) {
+//		return 1 + my_strlen(str + 1);
+//	}
+//	else
+//		return 0;
+//}
+
+//int my_strlen(const char* str) {
+//	assert(str);
+//	const char* start = str;
+//	while (*str != '\0') {
+//		str++;
+//	}
+//	return str - start;
+//}
 //
 //int main()
 //{
-//	struct Stu s1; //s1 是结构体变量
+//	char str[] = "abcdef";
+//	int len = my_strlen(str);
+//	printf("%d\n", len);
+//
 //	return 0;
 //}
 
-//调试：Ctrl+F7
-//struct Node
-//{
-//	int data;
-//	struct Node* next;
-//};
+
+//char* my_strcpy(char* dest, const char* src) {
+//	assert(dest && src);
+//	while (*src) {
+//		*dest = *src;
+//		dest++;
+//		src++;
+//	}
+//	*dest = *src; //此时src中还剩下\0，赋值给dest
+//	return dest;
+//}
+
+
+//char* my_strcpy(char* dest, const char* src) {
+//	char* start = dest;
+//	assert(dest && src);
+//	while (*dest++ = *src++) {
+//		;
+//	}
+//	return start;
+//}
 //
 //int main()
 //{
+//	char str1[] = "hawkeye";
+//	char str2[20] = "EEEEEEEEEEEEEEEE";
 //
+//	my_strcpy(str2, str1);
+//	printf("%s\n", str2);
 //	return 0;
 //}
 
+char* my_strcat(char* dest, const char* src) {
+	char* start = dest;
+	assert(dest && src);
+	//1. 目标空间中的\0
+	while (*dest) {
+		dest++;
+	}
+	//2. 追加内容到目标空间
+	while (*dest++ = *src++) {
+		;
+	}
+	return start;
+}
 
-//通讯录
-//1. 可以存放 1000 个用户的信息
-//2. 用户的信息包含：姓名、年龄、电话、住址、性别
-//3. 增加联系人
-//4. 删除联系人
-//5. 查找联系人
-//6. 修改联系人
-//7. 排序
+int main()
+{
+	char str1[30] = "hello";
+	char str2[] = "world";
+	my_strcat(str1, str2);
+	printf("%s\n", str1);
+
+	return 0;
+}
+
 
