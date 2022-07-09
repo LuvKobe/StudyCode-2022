e94226c24c0f67b95d9304c94cad96385d4e74d8
Edison <3078601436@qq.com>
2022-07-09 23:55:03 +0800

    Code Demo

---
+++ test_07_09/test.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+// #define ¶¨Òåºê
+
+//#define MAX(x, y) ((x)>(y)?(x):(y))
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int m = MAX(a, b);
+//	printf("%d\n", m);
+//	return 0;
+//}
+
+//#define DOUBLE(x) ((x)+(x))
+
+//#define PRINT(n) printf("the value of "#n" is %d\n", n);
+//
+//int main()
+//{
+//	int a = 10;
+//	PRINT(a);
+//
+//	return 0;
+//}
+
+
+//#define MALLOC(num, type) (type*)malloc(num*sizeof(type))
+//int mian()
+//{
+//	int* p1 = (int*)malloc(10 * sizeof(int));
+//
+//	int* p2 = MALLOC(10, int);
+//}
+
+int main()
+{
+	int i = 0;
+	int n = 0;
+#if 0
+	for (i = 0; i < 10; i++) {
+		printf("%d ", i);
+	}
+#endif
+	return 0;
+}
+
